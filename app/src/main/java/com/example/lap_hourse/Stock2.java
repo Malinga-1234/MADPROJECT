a084d8a783ea0a3b0811a9750a8ac98319005121
Lap.lk <malingaabi98@gmail.com>
2020-08-14 22:01:27 +0530

    MAD project

---
+++ app/src/main/java/com/example/lap_hourse/Stock2.java
@@ -0,0 +1,14 @@
+package com.example.lap_hourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Stock2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock2);
+    }
+}
