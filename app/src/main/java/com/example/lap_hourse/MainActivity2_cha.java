734bf77d8c22804ab86e90ef458b94fd3a01d347
chamodya99 <trcjayashanki1999@gmail.com>
2020-08-14 23:30:46 +0530

    adding_supplier with crud operation

---
+++ app/src/main/java/com/example/lap_hourse/MainActivity2_cha.java
@@ -0,0 +1,14 @@
+package com.example.lap_hourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2_cha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_cha);
+    }
+}
---
+++ app/src/main/java/com/example/lap_hourse/MainActivity3_cha.java
@@ -0,0 +1,14 @@
+package com.example.lap_hourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3_cha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity3_cha);
+    }
+}
